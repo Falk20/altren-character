cd0cd584207780fe0fa9f47ef0557df247237d2b
Falk20 <petruk0908@gmail.com>
2022-05-08 23:39:42 +0300

    added navbar

---
+++ src/components/navbar/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar color="primary" density="compact">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon @click="toggleSideMenu"></v-app-bar-nav-icon>
+    </template>
+
+    <v-app-bar-title>{{ $route.meta.title }}</v-app-bar-title>
+
+    <template v-slot:append></template>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { mapMutations } from "vuex";
+
+export default defineComponent({
+  name: "AltNavbar",
+
+  methods: {
+    ...mapMutations(["toggleSideMenu"]),
+  },
+});
+</script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,29 @@ import { createStore } from "vuex";
 
 import character from "./modules/character";
 
+export interface IState {
+  sideMenu: boolean;
+}
+
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
+  state(): IState {
+    return {
+      sideMenu: false,
+    };
+  },
+  getters: {
+    sideMenu(state: IState): boolean {
+      return state.sideMenu;
+    },
+  },
+  mutations: {
+    changeSideMenu(state, value): void {
+      state.sideMenu = value;
+    },
+    toggleSideMenu(state): void {
+      state.sideMenu = !state.sideMenu;
+    },
+  },
   actions: {},
   modules: {
     character,
