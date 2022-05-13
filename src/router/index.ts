024c249872da274286bdb7f8be8d04312bab29c2
Vasiliy Petruk <petruk0908@gmail.com>
2022-05-14 01:06:10 +0300

    Fix hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import StatusView from "../views/StatusView.vue";
 
 const routes: Array<RouteRecordRaw> = [
@@ -61,7 +61,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory("/altren-character/"),
+  history: createWebHashHistory("/altren-character/"),
   routes,
 });
 
