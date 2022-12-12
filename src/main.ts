076db0223c4643379c6b5d0da17bcddc88b6f83f
Falk20 <petruk0908@gmail.com>
2022-12-12 21:40:56 +0300

    добавил service-worker

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import Maska from "maska";
+import "./registerServiceWorker";
 
 loadFonts();
 
