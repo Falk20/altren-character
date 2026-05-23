909b914bf460e088a595e309d8b380aed1ddd067
Falk20 <petruk0908@gmail.com>
2026-05-24 00:25:43 +0300

    fix: json gen

--- src/components/side-menu/export-json.vue
+++ src/components/side-menu/export-json.vue
@@ -1,31 +1,18 @@
 <template>
-  <v-list
-    density="compact"
-    nav
-  >
-    <v-list-item
-      title="Экспорт"
-      @click="importJson"
-    ></v-list-item>
-    <v-list-item
-      title="Выход"
-      class="red"
-      @click="logOut"
-    ></v-list-item>
+  <v-list density="compact" nav>
+    <v-list-item title="Экспорт" @click="importJson"></v-list-item>
+    <v-list-item title="Выход" class="red" @click="logOut"></v-list-item>
   </v-list>
 </template>
 
 <script setup lang="ts">
 import { googleSignOut } from "@/firebase/config"
+import { generateCharlist } from "@/helpers/utils"
 import { useAppStore } from "@/store/stores/app"
-import { ref } from "vue"
 import { useRouter } from "vue-router"
 
 const router = useRouter()
 const appStore = useAppStore()
-
-// todo: починить, когда перепишу стор
-const character = ref('test')
 
 const logOut = async () => {
   await googleSignOut()
@@ -36,7 +23,7 @@ const logOut = async () => {
 }
 
 const importJson = () => {
-  const stateJson = JSON.stringify(character)
+  const stateJson = JSON.stringify(generateCharlist())
   const stateFile =
     "data:text/json;charset=utf-8," + encodeURIComponent(stateJson)
 
