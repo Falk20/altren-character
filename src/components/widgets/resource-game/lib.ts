fa75c6561bf8be09d8049eb06bf563e20e3e4f69
Falk20 <petruk0908@gmail.com>
2026-07-21 00:40:37 +0300

    feat: новая формула генерации рандомных чисел

--- src/components/widgets/resource-game/lib.ts
+++ src/components/widgets/resource-game/lib.ts
@@ -22,5 +22,20 @@ export const levelOptions: IOption<TLevel>[] = levels.map((level) => ({
   value: level,
 }))
 
-export const getRndNumber = (max: number) =>
-  max > 0 ? Math.floor(Math.random() * max) + 1 : 1
+export const getRndNumber = (max: number) => {
+  if (max <= 0) {
+    return 1
+  }
+
+  const array = new Uint8Array(1)
+  const limit = 256 - (256 % max)
+
+  while (true) {
+    crypto.getRandomValues(array)
+    const val = array[0]
+
+    if (val < limit) {
+      return (val % max) + 1
+    }
+  }
+}
